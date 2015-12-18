1a7b2be0e232860adfa6a55a3c7afbefa9332fa4
mgrcar <miha.grcar@ijs.si>
2015-12-18 10:47:36 +0100

    Player can now make first move

--- Optimizer.cs
+++ Optimizer.cs
@@ -36,44 +36,54 @@ namespace GameAi
             return val == 1 ? Player.Player1 : Player.Player2;
         }
 
-        public static void Play(IGameState state, int maxDepth = int.MaxValue)
+        public static void Play(IGameState state, bool playerStarts = true, int maxDepth = int.MaxValue)
         {
             Random rand = new Random();
+            bool skipFirstMove = playerStarts;
             while (true)
             {
-                double bestScore = double.MinValue;
-                List<int> bestMoves = new List<int>();
-                foreach (int move in state.AvailableMoves)
+                if (!skipFirstMove)
                 {
-                    state.MakeMove(move, Player.Player1);
-                    double score = state.Minimax(maxDepth);
-                    if (score > bestScore) { bestScore = score; bestMoves.Clear(); }
-                    if (score == bestScore) { bestMoves.Add(move); }
-                    state.UndoMove(move, Player.Player1);
+                    double bestScore = double.MinValue;
+                    List<int> bestMoves = new List<int>();
+                    foreach (int move in state.AvailableMoves)
+                    {
+                        state.MakeMove(move, Player.Player1);
+                        double score = state.Minimax(maxDepth);
+                        if (score > bestScore) { bestScore = score; bestMoves.Clear(); }
+                        if (score == bestScore) { bestMoves.Add(move); }
+                        state.UndoMove(move, Player.Player1);
+                    }
+                    state.MakeMove(bestMoves[rand.Next(bestMoves.Count)], Player.Player1);
+                    Console.WriteLine(state);
+                    if (state.Winner == Player.Player1)
+                    {
+                        Console.WriteLine("I won.");
+                        break;
+                    }
+                    else if (state.IsTerminal)
+                    {
+                        Console.WriteLine("It's a tie.");
+                        break;
+                    }
                 }
-                state.MakeMove(bestMoves[rand.Next(bestMoves.Count)], Player.Player1);
-                Console.WriteLine(state);
-                if (state.Winner == Player.Player1) 
-                { 
-                    Console.WriteLine("I won."); 
-                    break; 
-                }
-                else if (state.IsTerminal) 
-                { 
-                    Console.WriteLine("It's a tie."); 
-                    break; 
+                else
+                {
+                    skipFirstMove = false;
+                    Console.WriteLine(state);
                 }
                 Console.Write("Your move? ");
                 int playerMove = Convert.ToInt32(Console.ReadLine());
                 state.MakeMove(playerMove, Player.Player2);
                 if (state.Winner == Player.Player2) 
-                { 
+                {
                     Console.WriteLine(state); 
                     Console.WriteLine("You won."); 
                     break; 
                 }
                 else if (state.IsTerminal) 
-                { 
+                {
+                    Console.WriteLine(state); 
                     Console.WriteLine("It's a tie."); 
                     break; 
                 }

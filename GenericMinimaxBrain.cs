5caea3ea4eee1de8e649fed65d6d8d0c81db5d82
mgrcar <miha@sowalabs.com>
2018-04-10 22:39:29 +0200

    Now using alpha-beta pruning instead of simple Minimax

--- GenericMinimaxBrain.cs
+++ GenericMinimaxBrain.cs
@@ -37,42 +37,50 @@ namespace GameAiLib
             }
         }
 
+        private double AlphaBeta(IGame game, int depth, double alpha, double beta, Player player, bool maximize)
+        {
+            if (depth == 0 || game.IsTerminalState)
+            {
+                // 'maximize ? player : player.OtherPlayer()' is the same as 'player' that is passed to EvalGame
+                return MinimaxEval(game, maximize ? player : player.OtherPlayer());
+            }
+            if (maximize)
+            {
+                double v = double.MinValue;
+                foreach (int move in game.AvailableMoves)
+                {
+                    game.MakeMove(move, player);
+                    v = Math.Max(v, AlphaBeta(game, depth - 1, alpha, beta, player.OtherPlayer(), maximize: false));
+                    game.UndoMove(move, player);
+                    alpha = Math.Max(alpha, v);
+                    if (beta <= alpha) { break; }
+                }
+                return v;
+            }
+            else
+            {
+                double v = double.MaxValue;
+                foreach (int move in game.AvailableMoves)
+                {
+                    game.MakeMove(move, player);
+                    v = Math.Min(v, AlphaBeta(game, depth - 1, alpha, beta, player.OtherPlayer(), maximize: true));
+                    game.UndoMove(move, player);
+                    beta = Math.Min(beta, v);
+                    if (beta <= alpha) { break; }
+                }
+                return v;
+            }
+        }
+
         protected override double EvalGame(IGame game, Player player)
         {
+#if SIMPLE_MINIMAX
             return Minimax(game, /*maxDepth=*/int.MaxValue, player.OtherPlayer(), maximize: false);
+#else
+            return AlphaBeta(game, /*maxDepth=*/int.MaxValue, int.MinValue, int.MaxValue, player.OtherPlayer(), maximize: false);
+#endif
         }
 
         protected abstract double MinimaxEval(IGame game, Player player);
     }
-
-    //public static double AlphaBeta(this IGame node, int depth, double alpha, double beta, Player player = Player.Player2)
-    //{
-    //    if (depth == 0 || node.IsTerminalState) { return node.Score; }
-    //    if (player == Player.Player1)
-    //    {
-    //        double v = double.MinValue;
-    //        foreach (int move in node.AvailableMoves)
-    //        {
-    //            node.MakeMove(move, player); 
-    //            v = Math.Max(v, AlphaBeta(node, depth - 1, alpha, beta, Player.Player2));
-    //            node.UndoMove(move, player);
-    //            alpha = Math.Max(alpha, v);
-    //            if (beta <= alpha) { break; }
-    //        }
-    //        return v;
-    //    }
-    //    else
-    //    {
-    //        double v = double.MaxValue;
-    //        foreach (int move in node.AvailableMoves)
-    //        {
-    //            node.MakeMove(move, player); 
-    //            v = Math.Min(v, AlphaBeta(node, depth - 1, alpha, beta, Player.Player1));
-    //            node.UndoMove(move, player);
-    //            beta = Math.Min(beta, v);
-    //            if (beta <= alpha) { break; }
-    //        }
-    //        return v;
-    //    }
-    //}
 }
